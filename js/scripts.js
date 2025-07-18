44b2499644e7fb12e50a146504b1695ef5df0a08
VyacheslavVolkhin <vv@htmlworker.com>
2025-07-18 19:23:29 +0500

    add

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,57 @@ document.addEventListener("DOMContentLoaded", function() {
 	}
 
 
+	//js tabs
+	const tabsNav = document.querySelectorAll('.js-tabs-nav')
+	const tabsBlocks = document.querySelectorAll('.js-tab-block')
+	const tabsButtonTitle = document.querySelectorAll('.js-tab-title')
+	const tabsButtonContent = document.querySelectorAll('.js-tab-content')
+	function tabsActiveStart() {
+		for (iTab = 0; iTab < tabsBlocks.length; iTab++) {
+			if (tabsBlocks[iTab].classList.contains('active')) {
+				tabsBlocks[iTab].classList.remove('active')
+			}
+		}
+		for (i = 0; i < tabsNav.length; i++) {
+			let tabsNavElements = tabsNav[i].querySelectorAll('[data-tab]')
+			for (iElements = 0; iElements < tabsNavElements.length; iElements++) {
+				if (tabsNavElements[iElements].classList.contains('active')) {
+					let tabsNavElementActive = tabsNavElements[iElements].dataset.tab
+					for (j = 0; j < tabsBlocks.length; j++) {
+						if (tabsBlocks[j].dataset.tab.toString().indexOf(tabsNavElementActive) > -1) {
+							console.log(tabsBlocks[j].dataset.tab.toString().indexOf(tabsNavElementActive))
+							tabsBlocks[j].classList.add('active')
+						}
+					}
+				}
+			}
+		}
+		
+	}
+	for (i = 0; i < tabsButtonTitle.length; i++) {
+		tabsButtonTitle[i].addEventListener('click', function (e) {
+			this.classList.toggle('active')
+			e.preventDefault()
+			e.stopPropagation()
+			return false
+		})
+	}
+	for (i = 0; i < tabsNav.length; i++) {
+		tabsNav[i].addEventListener('click', function (e) {
+			if (e.target.closest('[data-tab]')) {
+				let tabsNavElements = this.querySelector('[data-tab].active')
+				tabsNavElements ? tabsNavElements.classList.remove('active') : false
+				e.target.closest('[data-tab]').classList.add('active')
+				tabsActiveStart()
+				e.preventDefault()
+				e.stopPropagation()
+				return false
+			}
+		})
+	}
+	tabsActiveStart()
+
+
 	//js popup wrap
 	const togglePopupButtons = document.querySelectorAll('.js-btn-popup-toggle')
 	const closePopupButtons = document.querySelectorAll('.js-btn-popup-close')
@@ -274,6 +325,66 @@ document.addEventListener("DOMContentLoaded", function() {
 		},
 	
 	});
+
+
+	//slider photos
+	const swiperSliderPhotos = new Swiper('.slider-photos .swiper', {
+		loop: false,
+		slidesPerView: 1,
+		spaceBetween: 0,
+		autoHeight: true,
+		speed: 400,
+		pagination: false,
+		autoplay: false,
+		navigation: {
+			nextEl: '.button-slider-photos-next',
+			prevEl: '.button-slider-photos-prev',
+		},
+		breakpoints: {
+			640: {
+				slidesPerView: 2,
+			},
+			1024: {
+				slidesPerView: 3,
+			},
+			1200: {
+				slidesPerView: 4,
+			},
+		},
+	
+	});	
+
+	//slider photos-srt
+	document.querySelectorAll('.slider-photos-srt').forEach(function(slider, i) {
+		new Swiper(slider.querySelector('.swiper'), {
+			loop: false,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: true,
+			speed: 400,
+			pagination: false,
+			autoplay: false,
+			navigation: {
+				nextEl: slider.querySelector('.button-slider-photos-srt-next'),
+				prevEl: slider.querySelector('.button-slider-photos-srt-prev'),
+			},
+			breakpoints: {
+				640: {
+					slidesPerView: 2,
+				},
+				1024: {
+					slidesPerView: 3,
+				},
+				1200: {
+					slidesPerView: 4,
+				},
+				1400: {
+					slidesPerView: 5,
+				},
+			},
+		});
+	});
+
 
 
 
